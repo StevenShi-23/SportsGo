e22edabd77e75d41488896748268bfb5b43328fa
gongzhen0128 <gong0040@e.ntu.edu.sg>
2017-04-10 16:58:00 +0800

    Add delete dialog to Favourite list view

--- app/src/main/java/com/example/sportsgo/sportsgo/fragment/FavoriteListFragment.java
+++ app/src/main/java/com/example/sportsgo/sportsgo/fragment/FavoriteListFragment.java
@@ -3,12 +3,12 @@ package com.example.sportsgo.sportsgo.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.app.Activity;
 import com.example.sportsgo.sportsgo.Activities.MainActivity;
 import com.example.sportsgo.sportsgo.MyApp;
 import com.example.sportsgo.sportsgo.R;
@@ -45,6 +45,8 @@ public class FavoriteListFragment extends MvpFragment<FavoriteListView, Favorite
         mListView.setAdapter(mAdapter);
         presenter.getAdapter(mAdapter);
         mListView.setOnItemClickListener(new FavoriteListFragment.ListItemClickListener());
+        mListView.setOnItemLongClickListener(new FavoriteListFragment.ListItemLongClickListener());
+        registerForContextMenu(mListView);
     }
     @Override
     public FavoriteListPresenter createPresenter(){
@@ -59,4 +61,36 @@ public class FavoriteListFragment extends MvpFragment<FavoriteListView, Favorite
             ((MainActivity)getActivity()).enterCompleteView(facility);
         }
     }
+
+    public class ListItemLongClickListener implements ListView.OnItemLongClickListener{
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            Facility facility = mAdapter.getItem(position);
+            Log.d("OnItemLongClick","In FavoriteListFragment");
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle("DELETE");
+            alert.setMessage("Remove from My Favourites? ");
+            alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //do your work here
+                    dialog.dismiss();
+
+                }
+            });
+            alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    dialog.dismiss();
+                }
+            });
+
+            alert.show();
+            return true;
+        }
+    }
+
 }
