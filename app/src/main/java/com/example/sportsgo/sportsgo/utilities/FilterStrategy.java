e0d1675b4bfaf11b949717ca8fef88a425f2194f
Ziji Shi <shi.ziji.sm@gmail.com>
2017-04-11 17:35:42 +0800

    -sort/ filter

---
+++ app/src/main/java/com/example/sportsgo/sportsgo/utilities/FilterStrategy.java
@@ -0,0 +1,64 @@
+package com.example.sportsgo.sportsgo.utilities;
+import com.example.sportsgo.sportsgo.model.Facility;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by StevenShi on 11/4/17.
+ */
+
+public interface FilterStrategy {
+    public List<Facility> FilterFacility (String cretaria, List<Facility> all_facilities, String DEFAULT_CRETARIA);
+}
+
+class FilterDrySports implements FilterStrategy{
+    @Override
+    public List<Facility> FilterFacility (String cretaria, List<Facility> all_facilities, String DEFAULT_CRETARIA){
+        List<Facility> Land_results = new ArrayList<Facility>();
+
+        if(cretaria == DEFAULT_CRETARIA){
+            return all_facilities;
+        }
+        else {
+            for (Facility f : all_facilities) {
+                if (!f.facilityName.toLowerCase().contains("swimming")&&!f.facilityName.toLowerCase().contains("water")) {
+                    Land_results.add(f);
+                }
+            }
+                return Land_results;
+        }
+    }
+}
+
+class FilterWaterSports implements FilterStrategy{
+    @Override
+    public List<Facility> FilterFacility (String cretaria, List<Facility> all_facilities, String DEFAULT_CRETARIA){
+        List<Facility> Water_results = new ArrayList<Facility>();
+
+        if(cretaria == DEFAULT_CRETARIA){
+            return all_facilities;
+        }
+        else {
+            for (Facility f : all_facilities) {
+                if (f.facilityName.toLowerCase().contains("swimming")||f.facilityName.toLowerCase().contains("water")) {
+                    Water_results.add(f);
+                }
+            }
+            return Water_results;
+        }
+    }
+}
+
+class filterContext {
+    private FilterStrategy filterBy;
+
+    public List<Facility> setFilterByStrategy(String cretaria, List<Facility> all_facilities, String DEFAULT_CRETARIA) {
+        if (cretaria == "dry") {
+            this.filterBy = new FilterDrySports();
+            return filterBy.FilterFacility(cretaria, all_facilities, DEFAULT_CRETARIA);
+        } else {
+            this.filterBy = new FilterDrySports();
+            return filterBy.FilterFacility(cretaria,all_facilities, DEFAULT_CRETARIA);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sportsgo/sportsgo/utilities/SortStrategy.java
@@ -0,0 +1,14 @@
+package com.example.sportsgo.sportsgo.utilities;
+
+import com.example.sportsgo.sportsgo.model.Facility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by StevenShi on 11/4/17.
+ */
+
+public interface SortStrategy {
+
+}
